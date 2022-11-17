0a71dd2fc0136178b0bc98972a9007f3292e9892
LFF <meerschweinchen5644@gmail.com>
2022-11-17 21:11:59 +0100

    can click buttons

--- app.js
+++ app.js
@@ -8,20 +8,29 @@ const {
 function IndexGame({state,actions}){
 	let gameScreen=state.gameScreen.map(
 		item=>
-			item==0
-			?
-				"_"
-			:
-				state.playerPointer[item-1]
+			item
+			?	state.playerPointer[item-1]
+			: 	"_"
 	);
-	let _=0;
+	let _0=0;
+	let _1=0;
 	let index=-1;
 	let tableChilds=[];
-	for(_ of Array(3)){
+	for(_0 of Array(3)){
 		let trChilds=[];
-		for(_ of Array(3)){
+		for(_1 of Array(3)){
 			index+=1;
-			trChilds.push(node_dom("td",null,[node_dom(`button[innerText=${gameScreen[index]}]`)]));
+			const i=index;
+			trChilds.push(
+				node_dom("td",null,[
+					node_dom(`button`,{
+						innerText:gameScreen[index],
+						onclick:()=>{
+							actions.editGameScreen([i,state.playerTurn+1])
+						},
+					})
+				]),
+			);
 		}
 		tableChilds.push(node_dom("tr",null,trChilds));
 	}
@@ -43,11 +52,26 @@ const model={
 			],
 		};
 	},
+	editGameScreen:(state,[index,value])=>({
+		...state,
+		gameScreen:[
+			...state.gameScreen.map((item,nowIndex)=>
+				nowIndex===index?value:item
+			),
+		],
+	}),
+	fillGameScreen:(state,fill=0)=>({
+		...state,
+		gameScreen: Array(9).fill(fill),
+	}),
 };
 init(()=>{
 	const [state,actions]=hook_model(model);
 	return[null,[
 		node_dom("h1[innerText=TicTacToe]"),
 		node(IndexGame,{state,actions}),
+		node_dom("button[innerText=Reset!]",{
+			onclick:()=>{actions.fillGameScreen(0);},
+		})
 	]];
 });
