b3e286519575bc045d40ae1800fa5f9924d80651
LFF <meerschweinchen5644@gmail.com>
2022-11-20 16:32:57 +0100

    fast spielbar

--- app.js
+++ app.js
@@ -5,26 +5,6 @@ const {
 	hook_model,
 }=lui;
 
-function IndexGame({state,actions}){return[
-	node_dom("div[className=xoxGrid]",null,
-		state.gameScreen
-		.map(item=>
-			item
-			?	state.playerPointer[item-1]
-			: 	state.playerPointer[2]
-		)
-		.map((item,index)=>
-			node_dom("button",{
-				innerText:item,
-				disabled:item!==state.playerPointer[2],
-				onclick:()=>{
-					actions.editGameScreen([index,state.playerTurn+1])
-				},
-			}),
-		)
-	),
-]}
-
 const model={
 	init:()=>({
 		playerNames:["Spieler 1","Spieler 2"],
@@ -45,18 +25,47 @@ const model={
 		),
 	}),
 
-	fillGameScreen:(state,fill=0)=>({
+	resetGameScreen:(state)=>({
 		...state,
-		gameScreen: Array(9).fill(fill),
+		gameScreen: Array(9).fill(0),
+	}),
+	toggelPlayer:(state)=>({
+		...state,
+		playerTurn:state.playerTurn?0:1,
 	}),
 };
+
+function IndexGame({state,actions}){return[
+	node_dom("p",{
+		innerText:`${state.playerNames[state.playerTurn]} ist dran und setzt ${state.playerPointer[state.playerTurn]}`,
+	}),
+	node_dom("div[className=xoxGrid]",null,
+		state.gameScreen
+		.map(item=>
+			item
+			?	state.playerPointer[item-1]
+			: 	state.playerPointer[2]
+		)
+		.map((item,index)=>
+			node_dom("button",{
+				innerText:item,
+				disabled:item!==state.playerPointer[2],
+				onclick:()=>{
+					actions.editGameScreen([index,state.playerTurn+1]);
+					actions.toggelPlayer();
+				},
+			}),
+		)
+	),
+]}
+
 init(()=>{
 	const [state,actions]=hook_model(model);
 	return[null,[
 		node_dom("h1[innerText=TicTacToe]"),
 		node(IndexGame,{state,actions}),
 		node_dom("button[innerText=Reset!]",{
-			onclick:()=>{actions.fillGameScreen(0);},
-		})
+			onclick:()=>{actions.resetGameScreen();},
+		}),
 	]];
 });
